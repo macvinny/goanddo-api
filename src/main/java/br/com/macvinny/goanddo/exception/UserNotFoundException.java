34b9e9001c0fbf758e765a91615d305d0dc3c10d
MacVinny <macvinny@protonmail.com>
2021-08-14 15:04:24 -0300

    Mudando a exceção para uso geral

--- src/main/java/br/com/macvinny/goanddo/exception/UserNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package br.com.macvinny.goanddo.exception;
-
-public class UserNotFoundException extends RuntimeException {
-    public UserNotFoundException(String message) {
-        super(message);
-    }
-}
