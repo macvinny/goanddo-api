87940464ac27bf09185dd6565b4f59216e82e1fa
MacVinny <macvinny@protonmail.com>
2021-10-25 21:57:11 -0300

    Refatorando entidades

--- src/main/java/br/com/macvinny/goanddo/model/Institution.java
+++ src/main/java/br/com/macvinny/goanddo/model/Institution.java
@@ -1,99 +1,33 @@
 package br.com.macvinny.goanddo.model;
 
+import lombok.*;
 import javax.persistence.*;
 import java.io.Serializable;
 
 @Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Institution implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(nullable = false, updatable = false)
     private Long id;
+    @Column(nullable = false)
     private String name;
-    private String phone;
+    @Column(nullable = false)
+    private String cnpj;
+    @Column(nullable = false)
+    private String description;
+    @Column(nullable = false)
     private String email;
-    private String address;
+    @Column(nullable = false)
+    private String phone;
+    @Column(nullable = false)
+    private String state;
+    @Column(nullable = false)
+    private String city;
+    private boolean active;
     private String imageUrl;
-    @Column(nullable = false, updatable = false)
-    private String institutionCode;
-
-    public Institution() {}
-
-    public Institution(String name, String phone, String email, String address, String imageUrl, String institutionCode) {
-        this.name = name;
-        this.phone = phone;
-        this.email = email;
-        this.address = address;
-        this.imageUrl = imageUrl;
-        this.institutionCode = institutionCode;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public String getImageUrl() {
-        return imageUrl;
-    }
-
-    public void setImageUrl(String imageUrl) {
-        this.imageUrl = imageUrl;
-    }
-
-    public String getInstitutionCode() {
-        return institutionCode;
-    }
-
-    public void setInstitutionCode(String institutionCode) {
-        this.institutionCode = institutionCode;
-    }
-
-    @Override
-    public String toString() {
-        return "Institution{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", phone='" + phone + '\'' +
-                ", email='" + email + '\'' +
-                ", address='" + address + '\'' +
-                ", imageUrl='" + imageUrl + '\'' +
-                ", institutionCode='" + institutionCode + '\'' +
-                '}';
-    }
 }
--- src/main/java/br/com/macvinny/goanddo/model/Person.java
+++ src/main/java/br/com/macvinny/goanddo/model/Person.java
@@ -1,97 +1,33 @@
 package br.com.macvinny.goanddo.model;
 
+import lombok.*;
 import javax.persistence.*;
 import java.io.Serializable;
 
 @Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Person implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(nullable = false, updatable = false)
     private Long id;
+    @Column(nullable = false)
     private String name;
-    private String phone;
+    @Column(nullable = false)
+    private String cpf;
+    @Column(nullable = false)
+    private String description;
+    @Column(nullable = false)
     private String email;
-    private String title;
+    @Column(nullable = false)
+    private String phone;
+    @Column(nullable = false)
+    private String state;
+    @Column(nullable = false)
+    private String city;
+    private boolean active;
     private String imageUrl;
-    @Column(nullable = false, updatable = false)
-    private String personCode;
-
-    public Person() {}
-
-    public Person(String name, String phone, String email, String title, String imageUrl, String personCode) {
-        this.name = name;
-        this.phone = phone;
-        this.email = email;
-        this.title = title;
-        this.imageUrl = imageUrl;
-        this.personCode = personCode;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    public String getEmail() { return email; }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getImageUrl() {
-        return imageUrl;
-    }
-
-    public void setImageUrl(String imageUrl) {
-        this.imageUrl = imageUrl;
-    }
-
-    public String getPersonCode() {
-        return personCode;
-    }
-
-    public void setPersonCode(String personCode) {
-        this.personCode = personCode;
-    }
-
-    @Override
-    public String toString() {
-        return "Person{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", phone='" + phone + '\'' +
-                ", email='" + email + '\'' +
-                ", title='" + title + '\'' +
-                ", imageUrl='" + imageUrl + '\'' +
-                ", personCode='" + personCode + '\'' +
-                '}';
-    }
 }
